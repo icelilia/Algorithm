11a95001ea5b0320b005576fa88f42f156fedeef
Spotted_Dog <792005029@qq.com>
2020-10-24 17:30:30 +0800

    静态化逆波兰表达式类

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,28 +3,32 @@ package main;
 import java.util.ArrayList;
 
 import arraySort.*;
+import reversePolishNotation.ReversePolishNotation;
 
 public class Main {
 
-	public static void main(String[] args) {
-		ArrayList<Integer> array = new ArrayList<Integer>();
-		array.add(5);
-		array.add(4);
-		array.add(1);
-		array.add(2);
-		array.add(7);
-		array.add(0);
-		array.add(3);
-		array.add(6);
-		array.add(9);
-		array.add(8);
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        array.add(5);
+        array.add(4);
+        array.add(1);
+        array.add(2);
+        array.add(7);
+        array.add(0);
+        array.add(3);
+        array.add(6);
+        array.add(9);
+        array.add(8);
 
-		ArraySort arraySort = new BucketSort(array, false);
-		ArrayList<Integer> res = arraySort.sort();
+        ArraySort arraySort = new BucketSort(array, false);
+        ArrayList<Integer> res = arraySort.sort();
 
-		for (Integer i : res) {
-			System.out.println(i);
-		}
-	}
+        for (Integer i : res) {
+            System.out.println(i);
+        }
+
+        String str = "a+b*(b+c/d)/g";
+        System.out.println(ReversePolishNotation.toPolishNotation(str));
+    }
 
 }
--- src/reversePolishNotation/ReversePolishNotation.java
+++ src/reversePolishNotation/ReversePolishNotation.java
@@ -5,109 +5,105 @@ import java.util.Stack;
 import bracketMatch.BracketMatch;
 
 public class ReversePolishNotation {
-	private Stack<Character> stack = new Stack<Character>();
-	private String str;
+    private static final Stack<Character> stack = new Stack<>();
 
-	public ReversePolishNotation(String string) {
-		str = string;
-	}
+    /**
+     * 1. 操作数直接输出
+     * 2. 操作符分三种情况：
+     * (1)空栈直接压栈
+     * (2)操作符优先级大于栈顶操作符，压栈
+     * (3)操作符优先级低于栈顶操作符，弹栈，直至操作符优先级大于栈顶操作符。然后压栈
+     * 3.操作符是左括号，压栈
+     * 4.操作符是右括号，弹栈，直至遇见左括号。然后将栈中的左括号弹出
+     * 5.输入的中缀表达式扫描完成，但是栈中仍存在操作符时，应将栈中的操作符弹出
+     *
+     * @return 转换完成的后缀表达式
+     */
+    public static String toPolishNotation(String str) {
+        stack.clear();
+        if (!new BracketMatch(str).isMatch()) {
+            return null;
+        }
+        char[] charArray = str.toCharArray();
+        char c;
+        StringBuilder result = new StringBuilder();
+        for (char value : charArray) {
+            c = value;
+            if (isOperator(c)) {
+                if (stack.isEmpty()) {
+                    stack.push(c);
+                } else {
+                    if (c == '+' || c == '-') {
+                        while (true) {
+                            if (stack.isEmpty()) {
+                                stack.push(c);
+                                break;
+                            }
+                            char temp = stack.pop();
+                            if (temp == '(' || temp == '[' || temp == '{') {
+                                stack.push(temp);
+                                stack.push(c);
+                                break;
+                            }
+                            result.append(temp);
+                        }
+                    } else if (c == '*' || c == '/') {
+                        while (true) {
+                            if (stack.isEmpty()) {
+                                stack.push(c);
+                                break;
+                            }
+                            char temp = stack.pop();
+                            if (temp == '(' || temp == '[' || temp == '{' || temp == '+' || temp == '-') {
+                                stack.push(temp);
+                                stack.push(c);
+                                break;
+                            }
+                            result.append(temp);
+                        }
+                    } else if (c == '(' || c == '[' || c == '{') {
+                        stack.push(c);
+                    } else if (c == ')') {
+                        while (true) {
+                            char temp = stack.pop();
+                            if (temp == '(') {
+                                break;
+                            } else {
+                                result.append(temp);
+                            }
+                        }
+                    } else if (c == ']') {
+                        while (true) {
+                            char temp = stack.pop();
+                            if (temp == '[') {
+                                break;
+                            } else {
+                                result.append(temp);
+                            }
+                        }
+                    } else if (c == '}') {
+                        while (true) {
+                            char temp = stack.pop();
+                            if (temp == '{') {
+                                break;
+                            } else {
+                                result.append(temp);
+                            }
+                        }
+                    }
+                }
+            } else {
+                result.append(c);
+            }
+        }
+        while (!stack.isEmpty()) {
+            result.append(stack.pop());
+        }
+        return result.toString();
+    }
 
-	/**
-	 * 1. 操作数直接输出 
-	 * 2. 操作符分三种情况： 
-	 *    (1)空栈直接压栈 
-	 *    (2)操作符优先级大于栈顶操作符，压栈
-	 *    (3)操作符优先级低于栈顶操作符，弹栈，直至操作符优先级大于栈顶操作符。然后压栈 
-	 * 3.操作符是左括号，压栈
-	 * 4.操作符是右括号，弹栈，直至遇见左括号。然后将栈中的左括号弹出 
-	 * 5.输入的中缀表达式扫描完成，但是栈中仍存在操作符时，应将栈中的操作符弹出
-	 * 
-	 * @return 转换完成的后缀表达式
-	 */
-	public String toPolishNotation() {
-		if (!new BracketMatch(str).isMatch()) {
-			return null;
-		}
-		char[] charArray = str.toCharArray();
-		char c;
-		String result = "";
-		for (int i = 0; i < charArray.length; i++) {
-			c = charArray[i];
-			if (isOperator(c)) {
-				if (stack.isEmpty()) {
-					stack.push(c);
-				} else {
-					if (c == '+' || c == '-') {
-						while (true) {
-							if (stack.isEmpty()) {
-								stack.push(c);
-								break;
-							}
-							char temp = stack.pop();
-							if (temp == '(' || temp == '[' || temp == '{') {
-								stack.push(temp);
-								stack.push(c);
-								break;
-							}
-							result = result + temp;
-						}
-					} else if (c == '*' || c == '/') {
-						while (true) {
-							if (stack.isEmpty()) {
-								stack.push(c);
-								break;
-							}
-							char temp = stack.pop();
-							if (temp == '(' || temp == '[' || temp == '{' || temp == '+' || temp == '-') {
-								stack.push(temp);
-								stack.push(c);
-								break;
-							}
-							result = result + temp;
-						}
-					} else if (c == '(' || c == '[' || c == '{') {
-						stack.push(c);
-					} else if (c == ')') {
-						while (true) {
-							char temp = stack.pop();
-							if (temp == '(') {
-								break;
-							} else {
-								result = result + temp;
-							}
-						}
-					} else if (c == ']') {
-						while (true) {
-							char temp = stack.pop();
-							if (temp == '[') {
-								break;
-							} else {
-								result = result + temp;
-							}
-						}
-					} else if (c == '}') {
-						while (true) {
-							char temp = stack.pop();
-							if (temp == '{') {
-								break;
-							} else {
-								result = result + temp;
-							}
-						}
-					}
-				}
-			} else {
-				result = result + c;
-			}
-		}
-		while (!stack.isEmpty()) {
-			result = result + stack.pop();
-		}
-		return result;
-	}
-
-	private boolean isOperator(char c) {
-		return c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')' || c == '[' || c == ']' || c == '{'
-				|| c == '}';
-	}
+    private static boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')' || c == '[' || c == ']' || c == '{'
+                || c == '}';
+    }
 }
